a759080a225f32e26a2fa65cb93e68d0518694c0
Esat GÖZCÜ <esatgozcu@gmail.com>
2023-03-13 20:58:33 +1000

    0.0.0
    Initial project

---
+++ AnimationExamples/AnimationExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationExamplesApp.swift
+//  AnimationExamples
+//
+//  Created by Esat Gözcü on 2023/03/13.
+//
+
+import SwiftUI
+
+@main
+struct AnimationExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationExamples/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimationExamples
+//
+//  Created by Esat Gözcü on 2023/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
